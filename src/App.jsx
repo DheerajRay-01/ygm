112d613d38d5fa9388e73e10fb5d77e34f4c9f5b
DheerajRay-01 <dheerajray2003@gmail.com>
2025-07-17 15:36:22 +0530

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Routes,Route } from 'react-router'
 import Layout from './components/pages/Layout'
 import Home from './components/pages/Home'
-import Loading from './components/sections/Loading'
 const App = () => {
 
   return (
